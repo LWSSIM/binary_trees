f062270e29348722ee06ef8b6d479a6b55aaa6b2
LWSSIM <wassim.elmnajja2@gmail.com>
2024-01-31 17:37:52 +0100

    added fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,9 +10,14 @@
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	binary_tree_t *new;
 
-	if (!new || !parent)
+	if (!parent)
+		return (NULL);
+
+	new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	if (!new)
 		return (NULL);
 
 	new->n = value;
