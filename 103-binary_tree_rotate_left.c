560980360e1dafceaad53e62168aa69489a70509
LWSSIM <wassim.elmnajja2@gmail.com>
2024-02-04 15:02:55 +0100

    fixed implementation

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -17,6 +17,19 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 	new_root = tree->right;
 	tree->right = new_root->left;
 	new_root->left = tree;
+	new_root->parent = tree->parent;
+
+	if (tree->right)
+		tree->right->parent = tree;
+
+	if (tree->parent)
+	{
+		if (tree->parent->left == tree)
+			tree->parent->left = new_root;
+		else
+		 tree->parent->right = new_root;
+	}
+	tree->parent = new_root;
 
 	return (new_root);
 }
